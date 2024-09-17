4135b43abe9a9611043ef97cb1a099772594af70
Rohith2k01 <rohith2k01@gmail.com>
2024-09-17 10:07:13 +0530

    change

--- backend/src/helpers/admin-api.js
+++ backend/src/helpers/admin-api.js
@@ -3,7 +3,6 @@ module.exports = {
   
     searchMovie: async (title,year) => {
       try {
-        // const searchMovieApi =`https://www.omdbapi.com/?t=${title}&y=${year}&apikey=175b2274`
         const searchMovieApi = 'http://www.omdbapi.com/?t=${title}&y=${year}&apikey=79cc66dd'
         console.log(searchMovieApi)
         return searchMovieApi
